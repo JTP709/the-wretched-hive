96a491f645b6644240c03df9bc9a44113626d57e
Jon Prell <JTP709@gmail.com>
2025-02-06 09:12:17 -0700

    update auth middleware to handle accessToken

--- server/src/middleware/authentication.ts
+++ server/src/middleware/authentication.ts
@@ -1,21 +1,22 @@
 import { NextFunction, Request, Response } from 'express';
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import { AuthRequest } from '../types/global';
+import { ACCESS_TOKEN_SECRET } from '../utils';
 
 const authentication = (req: Request, res: Response, next: NextFunction) => {
-  const token = req.cookies?.token || req.headers.authorization?.split(' ')[1];
+  const accessToken = req.cookies?.accessToken;
 
-  if (!token) {
-    res.status(403).json({ message: "No token provided" });
+  if (!accessToken) {
+    res.status(401).json({ message: "Unauthorized" });
     return;
   }
 
   try {
-    const decoded = jwt.verify(token, 'secret_key');
-    (req as AuthRequest).userId = (decoded as JwtPayload).id;
+    const decoded = jwt.verify(accessToken, ACCESS_TOKEN_SECRET);
+    (req as AuthRequest).userId = (decoded as JwtPayload).userId;
     next();
   } catch {
-    res.status(401).json({ message: "Unauthorized" })
+    res.status(403).json({ message: "Forbidden" })
   }
 }
 
