2b239c411c8e656921996faa837807ac7c1f8dae
Jon Prell <JTP709@gmail.com>
2025-02-06 09:04:30 -0700

    add generateCsrfToken util function

--- server/src/utils/token.ts
+++ server/src/utils/token.ts
@@ -1,4 +1,5 @@
 import jwt from 'jsonwebtoken';
+import crypto from 'crypto';
 
 export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET || 'access_secret';
 export const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET || 'refresh_secret';
@@ -9,4 +10,8 @@ export const generateAccessToken = (userId: number) => {
 
 export const generateRefreshToken = (userId: number) => {
   return jwt.sign({ userId }, REFRESH_TOKEN_SECRET, { expiresIn: "7d" });
+};
+
+export const generateCsrfToken = () => {
+  return crypto.randomBytes(32).toString("hex");
 };
