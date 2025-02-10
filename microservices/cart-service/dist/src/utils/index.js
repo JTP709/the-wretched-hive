cd9559750477e5195b9f2b0daeed840e91558f16
Jon Prell <JTP709@gmail.com>
2025-02-10 14:22:20 -0700

    remove dist directory from cart-services git

--- microservices/cart-service/dist/src/utils/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.handleErrors = void 0;
-const handleErrors = (res, err) => {
-    console.error(err);
-    res.status(500).json({ message: (err === null || err === void 0 ? void 0 : err.message) || 'Internal server error' });
-};
-exports.handleErrors = handleErrors;
