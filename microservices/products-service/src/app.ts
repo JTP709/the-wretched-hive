e9958353921427f0b484b0d3adfbedf1173ce515
Jon Prell <JTP709@gmail.com>
2025-02-11 09:45:22 -0700

    create products microservice and move code over from monolith

---
+++ microservices/products-service/src/app.ts
@@ -0,0 +1,44 @@
+import 'express-async-errors';
+import 'dotenv/config';
+import express from 'express';
+import sequelize from './models';
+import productRoutes from './routes';
+import { globalErrorHandler } from './middleware';
+import { main as gRpcServer } from "./grpc/server";
+
+const PORT = process.env.PORT || 4002;
+
+const app = express();
+
+app.use(express.json());
+app.use('/api/product', productRoutes);
+app.use(globalErrorHandler);
+
+(async function Main() {
+  try {
+    await sequelize.authenticate();
+    console.log('Connected to database');
+
+    if(process.env.NODE_ENV === 'development') {
+      await sequelize.sync();
+      // await sequelize.sync({ alter: true });
+      // use if error occurs WARNING: this will delete all data
+      // await sequelize.sync({ force: true })
+      console.log('All models were synchronized successfully');
+    }
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+
+    gRpcServer();
+
+    process.on('exit', () => {
+      sequelize.close()
+        .then(() => console.log('Closed the database connection successfully'))
+        .catch((err: Error) => console.error('Error closing the database connection', err));
+    });
+  } catch (err) {
+    console.error('An error occurred while starting the application', err);
+  }
+})()
---
+++ microservices/products-service/src/models/index.ts
@@ -0,0 +1,5 @@
+import sequelize from "./dbInitialize";
+import Product from "./Product"
+
+export default sequelize;
+export { Product };
