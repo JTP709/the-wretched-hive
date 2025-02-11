7c1387e87defd44dd7d2cf7a5ef40a3b5fc16dda
Jon Prell <JTP709@gmail.com>
2025-02-11 09:26:06 -0700

    fix refresh call missing csrf header

--- client/api/utils/shared.ts
+++ client/api/utils/shared.ts
@@ -1,20 +1,19 @@
 export const addRefreshToken = (url: string) => async (input: RequestInfo, init?: RequestInit) => {
-  const response = await fetch(input, {
+  const options: RequestInit = {
     ...init,
     credentials: 'include',
-  });
+  };
+  const response = await fetch(input, options);
 
   if (response.status === 401) {
     const refreshResponse = await fetch(url, {
       method: 'POST',
       credentials: 'include',
+      headers: init?.headers,
     });
 
     if (refreshResponse.ok) {
-      return await fetch(input,{
-        ...init,
-        credentials: 'include',
-      });
+      return await fetch(input, options);
     }
   }
 
