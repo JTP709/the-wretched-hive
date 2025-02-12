94aa9476cb05d7525e99c9f516b455a36efd7344
Jon Prell <JTP709@gmail.com>
2025-02-12 16:49:46 -0700

    sync users database

--- microservices/users-service/src/app.ts
+++ microservices/users-service/src/app.ts
@@ -1,11 +1,15 @@
-import 'dotenv/config';
-import path from 'path';
-import { loadPackageDefinition, Server, ServerCredentials } from '@grpc/grpc-js';
-import { loadSync } from '@grpc/proto-loader';
-// import sequelize from './models';
-import { get_user } from './controllers';
+import "dotenv/config";
+import path from "path";
+import {
+  loadPackageDefinition,
+  Server,
+  ServerCredentials,
+} from "@grpc/grpc-js";
+import { loadSync } from "@grpc/proto-loader";
+import sequelize from "./models";
+import { get_user, post_signup } from "./controllers";
 
-const PROTO_PATH = path.join(__dirname, './grpc', 'users.proto');
+const PROTO_PATH = path.join(__dirname, "./grpc", "users.proto");
 
 const packageDefinition = loadSync(PROTO_PATH, {
   keepCase: true,
@@ -16,36 +20,37 @@ const packageDefinition = loadSync(PROTO_PATH, {
 });
 const usersProto = loadPackageDefinition(packageDefinition).users as any;
 
-// const onShutdown = async () => {
-//   try {
-//     await sequelize.close();
-//     console.log('Closed the database connection successfully');
-//   } catch (err) {
-//     console.error('Error closing the database connection', err);
-//   } finally {
-//     process.exit();
-//   }
-// };
+const onShutdown = async () => {
+  try {
+    await sequelize.close();
+    console.log("Closed the database connection successfully");
+  } catch (err) {
+    console.error("Error closing the database connection", err);
+  } finally {
+    process.exit();
+  }
+};
 
 (async function Main() {
   try {
-    // await sequelize.authenticate();
-    // console.log('Connected to database');
+    await sequelize.authenticate();
+    console.log("Connected to database");
 
-    // if(process.env.NODE_ENV === 'development') {
-    //   await sequelize.sync();
-    //   // await sequelize.sync({ alter: true });
-    //   // use if error occurs WARNING: this will delete all data
-    //   // await sequelize.sync({ force: true })
-    //   console.log('All models were synchronized successfully');
-    // }
+    if (process.env.NODE_ENV === "development") {
+      // await sequelize.sync();
+      await sequelize.sync({ alter: true });
+      // use if error occurs WARNING: this will delete all data
+      // await sequelize.sync({ force: true })
+      console.log("All models were synchronized successfully");
+    }
 
-    // process.on('SIGINT', onShutdown);
-    // process.on('SIGTERM', onShutdown);
+    process.on("SIGINT", onShutdown);
+    process.on("SIGTERM", onShutdown);
 
     const server = new Server();
     server.addService(usersProto.UsersService.service, {
-      GetUser: get_user
+      GetUser: get_user,
+      PostSignUp: post_signup,
     });
 
     const bindAddress = "0.0.0.0:50054";
@@ -58,6 +63,6 @@ const usersProto = loadPackageDefinition(packageDefinition).users as any;
       console.log(`Cart gRPC server running at ${bindAddress}`);
     });
   } catch (err) {
-    console.error('An error occurred while starting the application', err);
+    console.error("An error occurred while starting the application", err);
   }
-})()
+})();
