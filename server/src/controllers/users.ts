19cd3410fa6d467f171503402833b0a3fe0cde17
Jon Prell <JTP709@gmail.com>
2025-02-13 16:20:53 -0700

    connect get users grpc call

--- server/src/controllers/users.ts
+++ server/src/controllers/users.ts
@@ -1,14 +1,13 @@
 import { Request, Response } from "express";
 import { AuthRequest } from "../types";
-import { getUser } from "../services";
-import { UsersActionType } from "../services/users";
-import { handleErrors } from "../utils";
+import { GetUser } from "../grpc/usersClient";
+import { UsersActionType } from "../types/enums";
 
 export const get_user = async (req: Request, res: Response) => {
   const userId = (req as AuthRequest).userId;
-  
-  const { type, data, message } = await getUser(userId);
-  switch(type) {
+
+  const { type, data, message } = await GetUser(userId);
+  switch (type) {
     case UsersActionType.SUCCESS:
       res.status(200).json(data);
       return;
